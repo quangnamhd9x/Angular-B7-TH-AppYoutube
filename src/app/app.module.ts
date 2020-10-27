348913d5ca8fb105ce64fb98cbbe56640e7e2c4a
QuangNam <quangnamhd9x@gmail.com>
2020-10-27 09:05:37 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    YoutubePlaylistComponent,
+    YoutubePlayerComponent,
+  ],
+  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
